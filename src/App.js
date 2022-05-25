48222c21833a29ee564d88e2e5ba79c846862e6a
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2022-05-25 12:53:58 +0400

    Was able to add the userContext and userReducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext, useReducer } from 'react';
 import Login from "./login/Login";
-import { loginUrl, getTokenFromUrl } from "./spotify"
+import { getTokenFromUrl } from "./spotify"
+import SpotifyWebApi from 'spotify-web-api-js';
+import { DataLayerContext } from '.';
+import reducer from './reducer';
 
+
+
+
+const spotify = new SpotifyWebApi()
 
 function App() {
   const [token, setToken] = useState("")
 
+  const {user, token_} = useContext(DataLayerContext)
+  const [state, dispatch] = useReducer(reducer, {user, token_})
+ 
   useEffect(() => {
     const obj = getTokenFromUrl()
     window.location.hash = ""
@@ -13,11 +23,30 @@ function App() {
 
     if(_token) {
       setToken(_token)
+
+      dispatch({
+        type : "SET_TOKEN",
+        token_ : _token
+      })
+
+      spotify.setAccessToken(_token)
+
+      spotify.getMe().then(user => {
+        dispatch({
+          type : "SET_USER",
+          user : user
+        })
+        
+      })
     }
-    
+  
   }, [])
 
   console.log(token)
+  console.log(state)
+  
+  
+ 
 
   return (
     <>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { initialState } from './reducer';
+
+export const DataLayerContext = createContext({})
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DataLayerContext.Provider value={initialState}>
+      <App />
+    </DataLayerContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
