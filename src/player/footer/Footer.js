508911fe4a66b536fbe9bc68e99d296539256280
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2022-05-26 23:24:23 +0400

    Worked on the footer

--- src/player/footer/Footer.js
+++ src/player/footer/Footer.js
@@ -1,9 +1,52 @@
 import "./footer.css"
+import PlayCircleOutlinedIcon from '@mui/icons-material/PlayCircleOutlined';
+import SkipNextOutlinedIcon from '@mui/icons-material/SkipNextOutlined';
+import SkipPreviousOutlinedIcon from '@mui/icons-material/SkipPreviousOutlined';
+import ShuffleIcon from '@mui/icons-material/Shuffle';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import { Grid, Slider, Box } from "@material-ui/core"
+import PlaylistPlayIcon from '@mui/icons-material/PlaylistPlay';
+import VolumeDownIcon from '@mui/icons-material/VolumeDown';
+
 
 const Footer = () => {
     return (
         <div className="footer">
-            <h2>I'm the Footer</h2>
+            <div className="footer-left">
+                < img src="https://www.okayplayer.com/wp-content/uploads/2022/05/kendrick-lamar-mr-morale-and-the-big-steppers-album-cover.jpeg" alt="" />
+               
+               <div className="footer-left-songInfo">
+                    <h4> Yeah !</h4>
+                    <p> Drake</p>
+               </div>
+            </div>
+
+            <div className="footer-center">
+                <ShuffleIcon className="footer-green"/>
+                <SkipPreviousOutlinedIcon className="footer-icon" />
+                <PlayCircleOutlinedIcon fontSize="large" className="footer-icon" />
+                <SkipNextOutlinedIcon className="footer-icon" />
+                <RepeatIcon className="footer-green"/>
+            </div>
+
+            <div className="footer-right">
+                <Grid container spacing={2}>
+                    <Grid item>
+                        < PlaylistPlayIcon />
+                    </Grid>
+
+                    <Grid item>
+                        < VolumeDownIcon />
+                    </Grid>
+
+                    <Grid item>
+                        < Box width={100}>
+                        < Slider size="small" defaultValue={70} aria-label="small" valueLabelDisplay="auto" />
+                        </Box>
+                    </Grid>
+                   
+                </Grid>
+            </div>
         </div>
     )
   }
